d113be331dee729061f22322c0454f40256f09a9
Avishaignd <avishaignd@gmail.com>
2021-07-28 13:23:30 +0300

    transactions WIP

--- src/components/Cart.js
+++ src/components/Cart.js
@@ -1,16 +1,23 @@
 import React, { useContext} from "react";
+import { Button } from "react-bootstrap";
+import { checkout } from "../lib/api";
 import { UserContext } from "../lib/context";
 import Product from "./Product";
 
 export default function Cart() {
   const user = useContext(UserContext);
+  const handleCheckout = async () => {
+      checkout(user)
+  }
+
   return (
     <>
+    <Button onClick={handleCheckout}>Proceed to checkout</Button>
       {user.user && user.user.cart ? (
         user.user.cart.map((product) => {
           return <Product key={product.id} props={product} />;
         })
-      ) : (
+        ) : (
         <h1>Your cart is empty</h1>
       )}
     </>
--- src/lib/api.js
+++ src/lib/api.js
@@ -9,12 +9,12 @@ export const getAllProducts = async () => {
 }
 
 export const postProduct = async (prodToAdd) => {
-    const response = await axios.post('http://localhost:5000/api/products', prodToAdd)
+    const response = await axios.post(baseURL+'/api/products', prodToAdd)
     console.log(response);
 }
 
 export const getUser = async (id) => {
-    const response = await axios.get(`http://localhost:5000/users/${id}`)
+    const response = await axios.get(baseURL+`/users/${id}`)
     console.log(response);
     const data = response.data
     console.log(data)
@@ -22,13 +22,19 @@ export const getUser = async (id) => {
 }
 
 export const sendProductToCart = async (data) => {
-    const response = await axios.post(`http://localhost:5000/api/users/add`, data)
+    const response = await axios.post(baseURL+`/api/users/add`, data)
     console.log(response);
     return response
 }
 
 export const removeProductFromCart = async (data) => {
-    const response = await axios.post(`http://localhost:5000/api/users/remove`, data)
+    const response = await axios.post(baseURL+`/api/users/remove`, data)
+    console.log(response);
+    return response
+}
+
+export const checkout = async (data) => {
+    const response = await axios.post(baseURL+'/api/users/checkout', data)
     console.log(response);
     return response
 }
