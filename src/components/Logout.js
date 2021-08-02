0d512e9b69269d5cc0432ca04ec2a9c523e256c0
Avishaignd <avishaignd@gmail.com>
2021-08-02 10:48:59 +0300

    logout fixes

--- src/components/Logout.js
+++ src/components/Logout.js
@@ -1,9 +1,13 @@
-import React from "react";
+import React, { useContext } from "react";
 import { Button } from "react-bootstrap";
+import { UserContext } from "../lib/context";
 
 export default function Logout() {
+  const user = useContext(UserContext)
+
   const handleLogout = () => {
-      window.location.reload();
+      user.setUser(null)
+      window.location.assign("https://shopping-cart-front.netlify.app")
   };
 
   return <Button onClick={handleLogout}>Log Out</Button>;
